f65f016306e9ee2d2d83d9a73290a940efa87e99
Poplavskij Vladimir <float45@gmail.com>
2024-10-03 19:33:39 +0300

    Start refactor basic

--- examples/js/basic.js
+++ examples/js/basic.js
@@ -1,60 +1,57 @@
 /* global jsPDF */
 function demoTwoPageDocument() {
-  var doc = new jsPDF();
+  const doc = new jsPDF();
   doc.text(20, 20, "Hello world!");
   doc.text(20, 30, "This is client-side Javascript, pumping out a PDF.");
   doc.addPage();
   doc.text(20, 20, "Do you like that?");
 
   // Save the PDF
-  doc.save("Test.pdf");
+  doc.save("demoTwoPageDocument.pdf");
 }
 
 function demoLandscape() {
-  var doc = new jsPDF("landscape");
+  const doc = new jsPDF("landscape");
   doc.text(20, 20, "Hello landscape world!");
 
   // Save the PDF
-  doc.save("Test.pdf");
+  doc.save("demoLandscape.pdf");
 }
 
 function demoFontSizes() {
-  var doc = new jsPDF();
+  const doc = new jsPDF();
   doc.setFontSize(22);
   doc.text(20, 20, "This is a title");
 
   doc.setFontSize(16);
   doc.text(20, 30, "This is some normal sized text underneath.");
 
-  doc.save("Test.pdf");
+  doc.save("demoFontSizes.pdf");
 }
 
 function demoFontTypes() {
-  var doc = new jsPDF();
+  const doc = new jsPDF();
 
   doc.text(20, 20, "This is the default font.");
 
-  doc.setFont("courier");
-  doc.setFontType("normal");
+  doc.setFont("courier", "normal");
   doc.text(20, 30, "This is courier normal.");
 
-  doc.setFont("times");
-  doc.setFontType("italic");
+  doc.setFont("times", "italic");
   doc.text(20, 40, "This is times italic.");
 
-  doc.setFont("helvetica");
-  doc.setFontType("bold");
+  doc.setFont("helvetica", "bold");
+
   doc.text(20, 50, "This is helvetica bold.");
 
-  doc.setFont("courier");
-  doc.setFontType("bolditalic");
+  doc.setFont("courier", "bolditalic");
   doc.text(20, 60, "This is courier bolditalic.");
 
-  doc.save("Test.pdf");
+  doc.save("demoFontTypes.pdf");
 }
 
 function demoTextColors() {
-  var doc = new jsPDF();
+  const doc = new jsPDF();
 
   doc.setTextColor(100);
   doc.text(20, 20, "This is gray.");
